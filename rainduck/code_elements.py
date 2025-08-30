20dc950fa84f23d6c715b7de1dd6f8cff921fa01
Lukáš Koma <koma.luky@gmail.com>
2025-08-30 08:53:46 +0200

    Add code block
    Code can now contain code block "{...something...}" with macro
    definition, but macros are not implemented yet.

--- rainduck/code_elements.py
+++ rainduck/code_elements.py
@@ -1,8 +1,9 @@
 from abc import ABCMeta, abstractmethod
+from collections import OrderedDict
 from typing import Any, Self
 
 from rainduck.errors import RainDuckInversionError, RainDuckSyntaxError
-from rainduck.tokens import Char, Number, Token
+from rainduck.tokens import Char, Number, Token, Word
 
 
 class _CodeElementMeta(ABCMeta):
@@ -20,7 +21,9 @@ class _CodeElementMeta(ABCMeta):
             else:
                 code_elements.append(cls)
 
-    def take(cls, code: list[Token]) -> "CodeElement | None":
+    def take(
+        cls, code: list[Token], parent: "CodeBlock | None" = None
+    ) -> "CodeElement | None":
         pass
 
 
@@ -46,14 +49,13 @@ class BrainFuck(CodeElement):
 
 class BrainFuckOperation(BrainFuck):
 
-    precedence = 10
     code: str
 
     def __init__(self, code: str) -> None:
         self.code = code
 
     @classmethod
-    def take(cls, code: list[Token]) -> Self | None:
+    def take(cls, code: list[Token], parent: "CodeBlock | None" = None) -> Self | None:
         fst = code[0]
         match fst:
             case Char(c) if c in "<>+-,.":
@@ -76,7 +78,6 @@ class BrainFuckOperation(BrainFuck):
 
 class BrainFuckLoop(BrainFuck):
 
-    precedence = 10
     code: list[CodeElement]
     line_pos: int | None
     char_pos: int | None
@@ -89,7 +90,9 @@ class BrainFuckLoop(BrainFuck):
         self.char_pos = char_pos
 
     @classmethod
-    def take(cls, tokens: list[Token]) -> Self | None:
+    def take(
+        cls, tokens: list[Token], parent: "CodeBlock | None" = None
+    ) -> Self | None:
         match tokens[0]:
             case Char("[", line_pos, char_pos):
                 del tokens[0]
@@ -109,7 +112,7 @@ class BrainFuckLoop(BrainFuck):
                             code.append(t)
                 else:
                     raise RainDuckSyntaxError("Missing ']'", line_pos, char_pos)
-                return cls(parse_list(code), line_pos, char_pos)
+                return cls(parse_list(code, parent), line_pos, char_pos)
         return None
 
     def transpile(self, inverse: bool = False) -> list["BrainFuck"]:
@@ -125,7 +128,6 @@ class BrainFuckLoop(BrainFuck):
 
 class Multiplication(CodeElement):  # Not implemented now, so corresponding test fails
 
-    precedence = 10
     num: int
     code: CodeElement
     line_pos: int | None
@@ -140,11 +142,13 @@ class Multiplication(CodeElement):  # Not implemented now, so corresponding test
         self.char_pos = char_pos
 
     @classmethod
-    def take(cls, code: list[Token]) -> "CodeElement | None":
+    def take(
+        cls, code: list[Token], parent: "CodeBlock | None" = None
+    ) -> "CodeElement | None":
         match code[0]:
             case Number(n, line_pos, char_pos):
                 del code[0]
-                return cls(n, _take_elem(code), line_pos, char_pos)
+                return cls(n, _take_elem(code, parent), line_pos, char_pos)
         return None
 
     def transpile(self, inverse: bool = False) -> list["BrainFuck"]:
@@ -160,17 +164,171 @@ class Multiplication(CodeElement):  # Not implemented now, so corresponding test
             return num * self.code.transpile(inv)
 
 
-def _take_elem(tokens: list[Token]) -> CodeElement:
+class Macro:
+
+    def __init__(
+        self, name: str, args: OrderedDict[str, CodeElement | None], code: CodeElement
+    ):
+        pass
+
+
+class CodeBlock(CodeElement):
+
+    macros: dict[str, Macro]
+    parent: "CodeBlock | None"
+    code: list[CodeElement]
+
+    def __init__(
+        self, tokens: list[Token], macro_defs: list[Token], parent: "CodeBlock | None"
+    ) -> None:
+        while macro_defs:
+            match macro_defs.pop(0):
+                case Word(name, line_pos, char_pos):
+                    if not macro_defs:
+                        raise RainDuckSyntaxError(
+                            "Expected macro definition.", line_pos, char_pos
+                        )
+                    args: OrderedDict[str, CodeElement | None] = OrderedDict()
+                    match macro_defs[0]:
+                        case Char("(", bracket_line_pos, bracket_char_pos):  # Take args
+                            del macro_defs[0]
+                            while macro_defs:
+                                token = macro_defs.pop(0)
+                                match token:
+                                    case Char(")"):
+                                        break
+                                if not macro_defs:
+                                    raise RainDuckSyntaxError(
+                                        "Macro arguments definition not finished.",
+                                        bracket_line_pos,
+                                        bracket_char_pos,
+                                    )
+                                match token:
+                                    case Word(arg_name):
+                                        match macro_defs.pop(0):
+                                            case Char(";" | ")"):
+                                                args[arg_name] = None
+                                            case Char("=", cp, lp):
+                                                if not macro_defs:
+                                                    raise RainDuckSyntaxError(
+                                                        "Expected default argument value.",
+                                                        cp,
+                                                        lp,
+                                                    )
+                                                args[arg_name] = _take_elem(
+                                                    macro_defs, self
+                                                )
+                                                if not macro_defs:
+                                                    raise RainDuckSyntaxError(
+                                                        "Macro arguments definition not finished.",
+                                                        bracket_line_pos,
+                                                        bracket_char_pos,
+                                                    )
+                                                match macro_defs[0]:
+                                                    case Char(";"):
+                                                        del macro_defs[0]
+                                                    case Char(")"):
+                                                        pass
+                                                    case t:
+                                                        raise RainDuckSyntaxError(
+                                                            "';' or ')' expected.",
+                                                            t.line_pos,
+                                                            t.char_pos,
+                                                        )
+                    if len(macro_defs) < 2:
+                        raise RainDuckSyntaxError(
+                            "Expected macro definition.", line_pos, char_pos
+                        )
+                    match macro_defs.pop(0):
+                        case Char("="):
+                            block = CodeBlock.take(macro_defs, self)
+                            if block is None:
+                                t2: Token = macro_defs[0]
+                                raise RainDuckSyntaxError(
+                                    "Code block expected.", t2.line_pos, t2.char_pos
+                                )
+                            self.macros[name] = Macro(name, args, block)
+        self.parent = parent
+        self.code = parse_list(tokens, self)
+
+    def transpile(self, inverse: bool = False) -> list["BrainFuck"]:
+        return sum(
+            [elem.transpile(inverse) for elem in self.code[:: -1 if inverse else 1]], []
+        )
+
+    @classmethod
+    def take(
+        cls, code: list[Token], parent: "CodeBlock | None" = None
+    ) -> "CodeElement | None":
+        match code[0]:
+            case Char("{", char_pos, line_pos):  # }
+                del code[0]
+                macro_defs: list[Token] = []
+                match code[0]:
+                    case Word("let", let_line_pos, let_char_pos):
+                        del code[0]
+                        brackets = 0
+                        while code:
+                            match code.pop(0), brackets:
+                                case Word("in"), 0:
+                                    break
+                                case Char("}", end_line_pos, end_char_pos), 0:
+                                    e = RainDuckSyntaxError(
+                                        "End of group before end of let-in block.",
+                                        end_line_pos,
+                                        end_char_pos,
+                                    )
+                                    e.add_pointer(let_line_pos, let_char_pos)
+                                    raise e
+                                case Char("{") as t, _:
+                                    brackets += 1
+                                    macro_defs.append(t)
+                                case Char("}") as t, _:
+                                    brackets -= 1
+                                    macro_defs.append(t)
+                                case t, _:
+                                    macro_defs.append(t)
+                        else:
+                            raise RainDuckSyntaxError(
+                                "End of file before end of let-lin block",
+                                let_char_pos,
+                                let_line_pos,
+                            )
+                tokens: list[Token] = []
+                brackets = 0
+                while code:
+                    match code.pop(0), brackets:
+                        case Char("}", end_line_pos, end_char_pos), 0:
+                            break
+                        case Char("{") as t, _:
+                            brackets += 1
+                            tokens.append(t)
+                        case Char("}") as t, _:
+                            brackets -= 1
+                            tokens.append(t)
+                        case t, _:
+                            tokens.append(t)
+                else:
+                    raise RainDuckSyntaxError(
+                        "End of file before end of let-lin block", char_pos, line_pos
+                    )
+                return cls(tokens, macro_defs, parent)
+        return None
+
+
+def _take_elem(tokens: list[Token], parent: "CodeBlock | None" = None) -> CodeElement:
     for elem_cls in code_elements:
-        elem = elem_cls.take(tokens)
+        elem = elem_cls.take(tokens, parent)
         if not (elem is None):
             return elem
     t = tokens[0]
     raise RainDuckSyntaxError("Unrecognized pattern", t.line_pos, t.char_pos)
 
 
-def parse_list(tokens: list[Token]) -> list[CodeElement]:
+def parse_list(
+    tokens: list[Token], parent: "CodeBlock | None" = None
+) -> list[CodeElement]:
     result = []
     while tokens:
-        result.append(_take_elem(tokens))
+        result.append(_take_elem(tokens, parent))
     return result
